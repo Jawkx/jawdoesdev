d13d9051a92da39df29684145a72fe8e22b2a833
jaw <jawkaixuan99@gmail.com>
2024-09-26 19:01:57 +0800

    remove redundant content when deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{html,js}", "./output/**/*.{html,js}"],
+    content: ["./output/**/*.{html,js}"],
     theme: {
         extend: {},
     },
